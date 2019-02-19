ed45d9e52b4eb6280a9113de50c8f5b076e9e0f5
Oluwatobi Oyejide <moyejide2016@gmail.com>
2019-02-19 11:43:24 +0100

    added error handler feature

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+// DOM Variables
 let gifBoxes = document.querySelectorAll("div.gif-box");
 let gifImgs = document.getElementsByTagName("img");
 let loaderIcon = document.getElementById("loader");
     loaderIcon.style.display = "none";
 let searchForm = document.forms["fname"];
 let searchInput = searchForm["search-name"];
-let giphyAPI = searchQuery => `http://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=ZOEonCGqKG0RpUfUGZOigxO8ZOOqxzjf&limit=6`;        
+let errorParagraph = document.getElementById("error-message");
+
+// API URL
+let giphyAPI = searchQuery => `http://api.giphy.com/v1/gifs/search?q=${searchQuery}&api_key=ZOEonCGqKG0RpUfUGZOigxO8ZOOqxzjf&limit=6`;
+
+// Search Control Functions
 let getSearchValue = () => {
         let searchValue =  searchInput.value;
             searchValue = searchValue.replace(/ /g, "+");
@@ -12,12 +18,15 @@ let getSearchValue = () => {
 
         return searchValue;
 }
+
 let createAnElement = (element) => {
         return document.createElement(element);
 }
+
 let appendElement = (parent, child) => {
         return parent.appendChild(child);
 }
+
 let displayGifs = (gifObj) => {
         let gifData = gifObj.data;
         for (let x=0; x < gifBoxes.length; x++){
@@ -31,6 +40,7 @@ let displayGifs = (gifObj) => {
         }
         loaderIcon.style.display = "none";
 }
+
 let clearGifBoxes = () => {
         for (let x=0; x < gifImgs.length; x++){
                 for (let i=0; i < gifBoxes.length; i++){
@@ -38,70 +48,29 @@ let clearGifBoxes = () => {
                 }
         }
 }
+
 searchForm.addEventListener("submit",(even) => {
         
         even.preventDefault();
-        if(gifImgs) {
-                clearGifBoxes();
-        };
+        if(gifImgs) { clearGifBoxes(); }
       
         let searchValue = getSearchValue();
         let url = giphyAPI(searchValue);
-        loaderIcon.style.display = "inline"
+        loaderIcon.style.display = "inline";
+
         fetch(url)
-        .then((response) => response.json())
-        .then((result) => {
-        
-                displayGifs(result)
+        .then((response) => {
+                if (response.ok) return response;
+
+                return Promise.reject(error)
+        })
+        .then( response => response.json() )
+        .then( result => displayGifs(result) )
+        .catch((error) => {
+                errorParagraph.innerHTML= `Sorry an error occurred!  Error: ${error.message}`;
+                loaderIcon.style.display = "none";
         });
-});    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
